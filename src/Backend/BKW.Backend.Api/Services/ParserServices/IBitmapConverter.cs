79f8bfa52e6b9d4363773c5251da939e9b2537b5
Péter Fónad <fonadpeti98@gmail.com>
2020-11-25 02:57:45 +0100

    [Backend]: Implement gRPC client services

---
+++ src/Backend/BKW.Backend.Api/Services/ParserServices/IBitmapConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BKW.Backend.Api.Services.ParserServices
+{
+    internal interface IBitmapConverter
+    {
+        Bitmap ConvertRgbToBitmap(int width, int height, (int r, int g, int b)[] data);
+    }
+}
