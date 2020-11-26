6f04343ff68b5d984cec94b14c811e95c91e4705
Antal Detre <adetrux@gmail.com>
2020-11-26 18:10:08 +0100

    [Backend]: Refactor disabling animation

--- src/Backend/BKW.Backend.Api/Services/IAnimationService.cs
+++ src/Backend/BKW.Backend.Api/Services/IAnimationService.cs
@@ -11,7 +11,7 @@ namespace BKW.Backend.Api.Services
         Task<ICollection<Animation>> GetAnimationsForPurchaser(string id, int count, string? search);
         Task<ICollection<Animation>> GetAnimationsForOwner(string id, int count, string? search);
         Task<Animation> CreateAnimation(Animation animation);
-        Task DisableAnimation(string id, Animation animation);
+        Task DisableAnimation(string id);
         Task Purchase(string purchaserId, string animationId);
     }
 }
