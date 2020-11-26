faa9b5d9f1ec8993c2303012312de9c5c4985307
Antal Detre <adetrux@gmail.com>
2020-11-26 22:45:29 +0100

    [Backend]: Implement download

--- src/Backend/BKW.Backend.Api/Services/IAnimationService.cs
+++ src/Backend/BKW.Backend.Api/Services/IAnimationService.cs
@@ -1,6 +1,7 @@
 ﻿using BKW.Backend.Domain.Models;
 using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace BKW.Backend.Api.Services
@@ -11,6 +12,7 @@ namespace BKW.Backend.Api.Services
         Task<Animation> GetAnimation(string id);
         Task<ICollection<Animation>> GetAnimationsForPurchaser(string id, int count, string? search);
         Task<ICollection<Animation>> GetAnimationsForOwner(string id, int count, string? search);
+        Task<MemoryStream> GetFile(string id);
         Task<Animation> CreateAnimation(Animation animation, IFormFile file);
         Task DisableAnimation(string id);
         Task Purchase(string purchaserId, string animationId);
